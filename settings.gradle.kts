494e5d04b41a048622bd779b132603808f1e57b3
spv <pvstrekalov@yandex.ru>
2025-02-18 17:02:34 +0400

    L13-creationalPatterns

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,7 @@ include("L11-Java8")
 
 include("L12-solid")
 
+include("L13-creationalPatterns")
 include("L14-behavioralPatterns")
 include("L15-structuralPatterns:demo")
 include("L15-structuralPatterns:homework")
